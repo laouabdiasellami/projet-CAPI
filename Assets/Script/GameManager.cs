e320031d40e3d01cd5afeba7706279f3cd80bb2a
Regoma <quentinbosia@yahoo.fr>
2024-12-16 14:16:03 +0100

    Régle de difficulté

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,6 +5,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
 
+    public string difficulty;
     public string[] playerListe;
     public string[] playerValue;
     public int currentPlayer = 0;
@@ -13,6 +14,7 @@ public class GameManager : MonoBehaviour
     public TaskListe myTaskListe = new TaskListe();
     [Space]
     public int currentTask =-1;
+    public int currentTurn = 0;
     [Space]
     public CardScriptableGameObject[] cardDeck;
 
@@ -26,13 +28,64 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         ReadJson();
+        myTaskListe.difficulty = difficulty;
         TaskUpdate();
         UiManager.instance.CardInit(cardDeck);
+        UiManager.instance.NextPlayer(playerListe[currentPlayer]);
+        
     }
 
     public void NextPlayer(CardScriptableGameObject card)
     {
+        playerValue[currentPlayer] = card.value;
+        if(currentPlayer+1 < playerListe.Length)
+        {
+            currentPlayer++;
+            UiManager.instance.NextPlayer(playerListe[currentPlayer]);
+        }
+        else
+        {
+            if(currentTurn ==0)
+            {
+                currentTurn++;
+                if(Unanimite(out string val))
+                {
+                    myTaskListe.taskListe[currentTask].taskDifficulty = val;
+                    currentPlayer = 0;
+                    currentTurn = 0;
+                    UiManager.instance.NextPlayer(playerListe[currentPlayer]);
+                    TaskUpdate();
+                }
+                else
+                {
+                    //Discution
+                    currentPlayer = 0;
+                    UiManager.instance.NextPlayer(playerListe[currentPlayer]);
+                }
+            }
+            else if(myTaskListe.difficulty == "Moyenne")
+            {
+                myTaskListe.taskListe[currentTask].taskDifficulty = Moyenne();
+                currentPlayer = 0;
+                currentTurn = 0;
+                UiManager.instance.NextPlayer(playerListe[currentPlayer]);
+                TaskUpdate();
+            }
 
+            /*
+            currentPlayer = 0;
+            UiManager.instance.NextPlayer(playerListe[currentPlayer]);
+            int val = 0;
+            foreach (string value in playerValue)
+            {
+                val += int.Parse(value);
+            }
+            myTaskListe.taskListe[currentTask].taskDifficulty = val.ToString();
+
+            TaskUpdate();
+            */
+        }
+        
     }
 
     public void TaskUpdate()
@@ -41,14 +94,12 @@ public class GameManager : MonoBehaviour
             currentTask++;
         else
         {
+            WriteJson();
             Debug.Log("Task liste ended");
             return;
         }
 
         UiManager.instance.DisplayTask(currentTask+1, myTaskListe.taskListe[currentTask].taskName);
-
-
-
     }
 
     public void ReadJson()
@@ -63,6 +114,32 @@ public class GameManager : MonoBehaviour
     }
 
 
+    public bool Unanimite(out string result)
+    {
+        foreach (string value in playerValue)
+        {
+            if (value != playerValue[0])
+            {
+                result = null;
+                return false;
+            }
+        }
+        result = playerValue[0];
+        return true;    
+    }
+
+    public string Moyenne()
+    {
+        float result = 0;
+        foreach (string value in playerValue)
+        {
+            if(value != "Cafée?" && value != "?")
+                result += int.Parse(value);
+        }
+        result /= playerListe.Length;
+        return result.ToString();
+    }
+
 
 
     [System.Serializable]
@@ -75,6 +152,7 @@ public class GameManager : MonoBehaviour
     [System.Serializable]
     public class TaskListe
     {
+        public string difficulty;
         public Task[] taskListe;
     }
 }
